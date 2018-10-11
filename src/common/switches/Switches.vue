527853ad94afb6b99545bdd2b86398b71f2080ce
poisonit <poisonit@163.com>
2018-10-11 10:01:44 +0800

    common switches

---
+++ src/common/switches/Switches.vue
@@ -0,0 +1,65 @@
+<template>
+	<ul class="switches">
+		<li 
+			class="switch-item"
+			v-for="(item, index) of switches"
+			:key="index"
+			@click="switchItem(index)"
+			:class="{'active': currentIndex === index, 'small-font': smallFont === true}"
+
+		>
+			<span>{{item.name}}</span>
+		</li>
+	</ul>
+</template>
+
+<script>
+	export default {
+		props: {
+			currentIndex: {
+				type: Number,
+				default: 0
+			},
+			switches: {
+				type: Array,
+				default: []
+			},
+			smallFont: {
+				type: Boolean,
+				default: false
+			}
+		},
+		name: 'Switches',
+		methods: {
+			switchItem (index) {
+				this.$emit('switch', index)
+			}
+		}
+		
+	}
+</script>
+
+<style lang="stylus" scoped>
+	@import "~assets/stylus/variable.styl"
+	.switches
+		position relative
+		z-index 200
+		background $color-background
+		display flex
+		align-items center
+		width 100%
+		.switch-item
+			flex 1
+			text-align center
+			padding 15px
+			font-size $font-size-medium-x
+			color $color-text
+			border-bottom 1px solid transparent
+			&.small-font
+				padding 14px 5px
+				font-size $font-size-small
+			&.active
+				color $color-theme-f
+				border-bottom-color $color-theme-f 		
+				
+</style>
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,16 +1,37 @@
 <template>
 	<div class="home">
 		<home-header></home-header>
+		<switches 
+			:currentIndex="currentIndex" 
+			:switches="switches" 
+			@switch="switchItem"
+		></switches>
 	</div>
 </template>
 
 
 <script>
 	import HomeHeader from './components/Header'
+	import Switches from 'common/switches/Switches'
 	export default {
 		name: 'Home',
+		data () {
+			return {
+				currentIndex: 0,
+				switches: [
+					{ name: '正在热映' },
+					{ name: '即将上映' }
+				]
+			}
+		},
+		methods: {
+			switchItem (index) {
+				this.currentIndex = index
+			}
+		},
 		components: {
-			HomeHeader
+			HomeHeader,
+			Switches
 		}
 	}
 </script>
